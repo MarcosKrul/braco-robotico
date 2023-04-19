a211e5437f526e10a87561b15709c8da380b31d9
Marcos Renan Krul <marcos7renan@hotmail.com>
2023-04-19 14:15:23 -0300

    handle uart received data

--- sketch_main/sketch_main.ino
+++ sketch_main/sketch_main.ino
@@ -1,10 +1,11 @@
 #include <Servo.h>
 
 #define DEBUG 1
-
+#define CONST_UART_BUFFER_SIZE 24
 #define PIN_SERVO_BASE 9
 #define PIN_SERVO_MID 10
 #define PIN_SERVO_HAND 11
+
 
 class ServoMotor {
   
@@ -51,9 +52,17 @@ public:
   }
 };
 
+
+char buffer2serialData[CONST_UART_BUFFER_SIZE];
+byte angle_base, angle_mid, angle_hand;
 ServoMotor base = ServoMotor(90, 0, 180);
 ServoMotor mid = ServoMotor(90, 0, 90);
 ServoMotor hand = ServoMotor(90, 0, 180);
+
+
+void uartLoop(void(*)());
+void handleUartReceivedData();
+
 
 void setup() {
   Serial.begin(9600);
@@ -64,4 +73,46 @@ void setup() {
 }
 
 void loop() {
+  uartLoop(handleUartReceivedData);
+  delay(50);
+}
+
+void uartLoop(void(*cb)()) {
+  int static index = 0;
+  bool static hasData = false;
+
+  if (Serial.available()) {
+    hasData = true;
+    buffer2serialData[index] = Serial.read();
+    if ((index += 1) >= CONST_UART_BUFFER_SIZE) index = CONST_UART_BUFFER_SIZE-1; 
+  }
+  else {
+    if (hasData) {
+      buffer2serialData[index] = '\0';
+      cb();
+    }
+    index = 0;
+    hasData = false;
+  }
+}
+
+void handleUartReceivedData() {
+  char *token = strtok(buffer2serialData, ","); 
+  angle_base = atoi(token);
+
+  token = strtok(NULL, ","); 
+  angle_mid = atoi(token); 
+  
+  token = strtok(NULL, ","); 
+  angle_hand = atoi(token); 
+  
+#if DEBUG
+  Serial.println();
+  Serial.print("Valores recebidos pela serial: ");
+  Serial.print(angle_base);
+  Serial.print(", ");
+  Serial.print(angle_mid);
+  Serial.print(", ");
+  Serial.println(angle_hand);
+#endif
 }
